c85a844b8d8994df6791eba50a06544eb7b2eabe
Josh <lekgold@gmail.com>
2024-01-29 23:07:09 -0500

    Adding updated art assets

--- src/components/BingoTile.vue
+++ src/components/BingoTile.vue
@@ -5,7 +5,7 @@
 </template>
   
 <script lang="ts">
-import { defineComponent, PropType, } from 'vue';
+import { defineComponent, type PropType } from 'vue';
 import { useSelectedTileStore } from '@/stores/selectedTile';
 
 
